c825b5d2ac92d64b4c70ff16281f090302ad430c
范智阔 <fzk@choicesoft.com.cn>
2020-09-10 18:34:21 +0800

    增加正式环境socket

--- src/socket.ts
+++ src/socket.ts
@@ -2,9 +2,12 @@ import * as vscode from "vscode";
 import { connect } from "socket.io-client";
 
 export function registerNotification(main: () => void) {
-  const io = connect("http://10.10.12.87:20208/choicefe");
+  const io = connect("http://office.choicesaas.cn/choicefe", {
+    reconnectionAttempts: 10000,
+    reconnectionDelay: 1000 * 10 * 60,
+  });
 
-  io.on("update", (data: string) => {
+  io.io.on("update", (data: string) => {
     main();
     vscode.window.showInformationMessage(data);
   });
